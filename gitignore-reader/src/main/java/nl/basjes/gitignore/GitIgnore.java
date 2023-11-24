e68ec6976033c9602cd78a1f7f5967f6cc856683
Niels Basjes <niels@basjes.nl>
2023-11-24 10:42:19 +0100

    docs: Typo

--- gitignore-reader/src/main/java/nl/basjes/gitignore/GitIgnore.java
+++ gitignore-reader/src/main/java/nl/basjes/gitignore/GitIgnore.java
@@ -121,7 +121,7 @@ public class GitIgnore extends GitIgnoreBaseVisitor<Void> {
                     //    logs/debug.log
                     //    logs/important.log
                     // Documentation:
-                    //    Wait a minute! Shouldn't logs/important.logbe negated in the example on the left
+                    //    Wait a minute! Shouldn't logs/important.log be negated in the example on the left
                     //    Nope! Due to a performance-related quirk in Git, you can not negate a file that
                     //    is ignored due to a pattern matching a directory
 
