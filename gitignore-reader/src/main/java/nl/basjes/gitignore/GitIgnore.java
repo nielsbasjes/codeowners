2fb5d2f50e5f4329c3250a56a9a1310222312d70
Niels Basjes <niels@basjes.nl>
2024-01-14 13:05:28 +0100

    fix: Error logging

--- gitignore-reader/src/main/java/nl/basjes/gitignore/GitIgnore.java
+++ gitignore-reader/src/main/java/nl/basjes/gitignore/GitIgnore.java
@@ -92,7 +92,7 @@ public class GitIgnore {
                 }
             }
         } catch (IOException io) {
-            LOG.error("Got an IOException while reading the gitignore file", io);
+            LOG.error("Got an IOException while reading the gitignore file content: {}", io.getMessage());
         }
 
     }
