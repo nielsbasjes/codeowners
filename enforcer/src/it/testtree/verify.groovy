ee4ddf42295ba902dce18b018384da6618a4aef4
Niels Basjes <niels@basjes.nl>
2024-10-05 20:35:02 +0200

    test: Make integration test much stricter

--- enforcer/src/it/testtree/verify.groovy
+++ enforcer/src/it/testtree/verify.groovy
@@ -20,4 +20,46 @@ assert file.exists()
 
 String text = file.getText( "utf-8" )
 
+assert text.contains("[INFO] Using GitIgnore : \${baseDir}/.gitignore")
+assert text.contains("[INFO] Using GitIgnore : \${baseDir}/dir1/.gitignore")
+assert text.contains("[INFO] Using GitIgnore : \${baseDir}/dir3/.gitignore")
+
+// These exist but are ignored so they may not be read.
+assert !text.contains("[INFO] Using GitIgnore : \${baseDir}/dir4/.gitignore")
+assert !text.contains("[INFO] Using GitIgnore : \${baseDir}/dir4/subdir/.gitignore")
+assert !text.contains("[INFO] Using GitIgnore : \${baseDir}/dir4/subdir/subdir/.gitignore")
+
+assert text.contains("[INFO] Using CODEOWNERS: \${baseDir}/docs/CODEOWNERS")
+
+assert text.contains("[INFO] Approvers for: \${baseDir}/ : [@integrationtest]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/.gitignore : [@integrationtest]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/.mvn/ : [@integrationtest]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/docs/CODEOWNERS : [@documentation]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir1/ : [@integrationtest]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir1/.gitignore : [@dir1project]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir1/dir1.md : [@dir1project]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir2/ : [@integrationtest]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir2/dir2.txt : [@txt]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir2/file2.log : [@log]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir3/ : [@integrationtest]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir3/.gitignore : [@dir3project]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir3/dir3.txt : [@dir3project]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/dir3/file3.log : [@dir3project]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/invoker.properties : [@integrationtest]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/pom.xml : [@integrationtest]")
+assert text.contains("[INFO] Approvers for: \${baseDir}/verify.groovy : [@integrationtest]")
+
+// Make sure NONE of the ignored files get an approver
+
+assert !text.contains("[INFO] Approvers for: \${baseDir}/dir1/dir1.log")
+assert !text.contains("[INFO] Approvers for: \${baseDir}/dir1/dir1.txt")
+assert !text.contains("[INFO] Approvers for: \${baseDir}/dir1/file1.log")
+assert !text.contains("[INFO] Approvers for: \${baseDir}/dir2/dir2.log")
+assert !text.contains("[INFO] Approvers for: \${baseDir}/dir2/dir2.md")
+assert !text.contains("[INFO] Approvers for: \${baseDir}/dir3/dir3.log")
+assert !text.contains("[INFO] Approvers for: \${baseDir}/dir3/dir3.md")
+assert !text.contains("[INFO] Approvers for: \${baseDir}/dir4/") // Which matches everything under dir4
+assert !text.contains("[INFO] Approvers for: \${baseDir}/README.md")
+assert !text.contains("[INFO] Approvers for: \${baseDir}/root.md")
+
 return true
